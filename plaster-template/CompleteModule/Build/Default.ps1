b796dfe8388908cb363f8effd5ceced76be369e6
LIENHARD Laurent <laurent.lienhard@outlook.com>
2018-10-05 22:53:23 +0200

    Build script

---
+++ plaster-template/CompleteModule/Build/Default.ps1
@@ -0,0 +1,18 @@
+﻿properties {
+    $ModuleName = "<%= $PLASTER_PARAM_ModuleName %>"
+    $OriginePath = split-path $PSScriptRoot
+ }
+
+task Init -description "Initialisation" {
+   #Write-Host "==> ModuleName : " $ModuleName 
+   Write-host "Test de la présence du module"
+   if (!(Get-Module -ListAvailable -Name $ModuleName)) {
+    .\$OriginePath\Deploy\launch_deploy.ps1
+}
+}
+ 
+task Compile -description "Compiler le code" {
+   Write-Host "==> OriginePath : " $OriginePath
+}
+  
+task default -depends init, compile -description "lancer toutes les tasks"
